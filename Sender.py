cfe404b5ac95105cce78ba130ef152fca67f4d1c
ConanKapoor <kapoor.shivam88@gmail.com>
2017-10-26 23:57:22 +0530

    Symmetric Encryption done using caesar cipher

--- Sender.py
+++ Sender.py
@@ -3,6 +3,7 @@
 
 import uuid
 import hashlib
+from caesarcipher import CaesarCipher
 
 # Function to compute gcd of 2 numbers
 def gcd(a,b):
@@ -17,21 +18,28 @@ def modInverse(a,b):
         if ((a*i) % b) == 1:
             return x
 
+# Function to perform hashing using  hashlib and uuid
 def hashing(word):
     # uuid is used to generate a random number
     salt = uuid.uuid4().hex
     return hashlib.sha256(salt.encode() + word.encode()).hexdigest() + ':' + salt
 
+# Function to input data
 def input_data():
     p = int(input("Please Enter value of p [Prime number. eg. 17]: "))
     q = int(input("Please Enter value of q [Prime number. eg. 11]: "))
+
+    # Race condition
     if p == q:
         print("Value of p and q can't be equal. Please use different prime numbers.")
         input_data()
         exit()
-    msz = input("Please enter the data to be messaged.")
-    return p,q,msz
 
+    # Common key to be shared between respondents for symmetric communication.
+    common_key = input("Please enter the common key to be shared: ")
+    return p,q,common_key
+
+# RSA Key generation process
 def RSAKeyGeneration(p,q):
     n = p * q
     totient_func = (p-1)*(q-1)
@@ -53,7 +61,18 @@ def RSAKeyGeneration(p,q):
     print("Private Key here is - PR(%s,%s)" %(d,n))
     return n,e,d
 
-def Encryption(e,n,msz):
-    Cipher = (msz**e) % n
+# RSA Encryption process
+def RSAEncryption(e,n,common_key):
+    Cipher = (common_key**e) % n
     print("Cipher text generated is: %s", %(Cipher))
     return Cipher
+
+# Symmetric Encryption using shared common key
+def SymmetricEncryption(common_key):
+    msz = input("Please enter the message to be shared: ")
+    hashed_msz = hashing(msz)
+    print("Hash for message given is: %s" %(hashed_msz))
+
+    cipher = CaesarCipher(msz, offset= common_key)
+    encoded_msz = cipher.encoded
+    print("Symmetrically encrypted data is: %s" %(encoded_msz))
